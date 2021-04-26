08a8d3e4ee4f391e89fa560962ba0fa13e35d071
New-Byte <prasadjoshi9969@gmail.com>
2021-04-26 19:12:16 +0530

    V0.0.2-0.9

--- Vision/test/test1.py
+++
@@ -1,4 +0,0 @@
-import sys  
-print(type(sys.argv))  
-print('The command line arguments are:')  
-print(sys.argv[1])
--- Vision/test/test2.py
+++ Vision/test/test2.py
@@ -1,11 +1,19 @@
-import getopt  
-import sys  
-argv = sys.argv[1:]  
-try:  
-    opts, args = getopt.getopt(argv, 'hm:d', ['help', 'my_file='])  
-    print(opts)  
-    print(args)  
-except getopt.GetoptError:  
-    # Print a message or do something useful  
-    print('Something went wrong!')  
-    sys.exit(2)  
+import pyttsx3
+engine = pyttsx3.init()
+
+# Voice IDs pulled from engine.getProperty('voices')
+# These will be system specific
+hi_voice_id_f = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\MSTTS_V110_hiIN_KalpanaM"
+hi_voice_id_m = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\MSTTS_V110_hiIN_HemantM"
+
+# Use female English voice
+engine.setProperty('voice', hi_voice_id_m)
+engine.say('नमस्ते...')
+engine.say('हैलो ... मैं vision हूं')
+
+# Use female Russian voice
+engine.setProperty('voice', hi_voice_id_f)
+engine.say('नमस्ते...')
+engine.say('हाय ... मैं wanda हूं')
+
+engine.runAndWait()
